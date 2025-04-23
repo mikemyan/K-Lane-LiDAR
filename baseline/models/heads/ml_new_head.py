74a21b1ba935ba63381c6b06a2453e7980d99e66
mikemyan <mikeyan1997@gmail.com>
2025-04-23 11:44:07 -0400

    added a baseline new head ml_new_head.py (relevant config change as well)

---
+++ baseline/models/heads/ml_new_head.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from ..registry import HEADS
+
+@HEADS.register_module
+class LightweightConv2dHead(nn.Module):
+    def __init__(self, in_channels=32, num_cls=6, **kwargs):
+        super().__init__()
+        self.num_cls = num_cls
+        self.conf_head = nn.Conv2d(in_channels, 1, kernel_size=1)
+        self.cls_head = nn.Conv2d(in_channels, num_cls, kernel_size=1)
+
+    def forward(self, x):
+        # x: [B, C, H, W]
+        conf = self.conf_head(x)  # [B, 1, H, W]
+        cls = self.cls_head(x)    # [B, num_cls, H, W]
+        return {'conf': conf, 'cls': cls}
+
+    def loss(self, out, batch, loss_type=None):
+        # Example: binary cross-entropy for conf, cross-entropy for cls
+        conf_pred = out['conf']
+        cls_pred = out['cls']
+        conf_gt = batch['label'][:, 1:2, :, :]  # Adjust to your label format
+        cls_gt = batch['label'][:, 0, :, :]     # Adjust to your label format
+
+        conf_loss = F.binary_cross_entropy_with_logits(conf_pred, conf_gt.float())
+        cls_loss = F.cross_entropy(cls_pred, cls_gt.long())
+        return {'conf_loss': conf_loss, 'cls_loss': cls_loss, 'loss': conf_loss + cls_loss}
