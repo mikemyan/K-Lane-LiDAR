58c34ce781fd1fe2f0050fd5c5d4ad32e492766d
mikemyan <mikeyan1997@gmail.com>
2025-04-24 02:34:21 -0400

    Added gaussian kernel and dropout in Performer

--- baseline/models/backbone/ml_transformer.py
+++ baseline/models/backbone/ml_transformer.py
@@ -3,9 +3,25 @@ from torch import nn
 import torch.nn.functional as F
 from einops import rearrange
 
+# implementing dropout to prevent overfitting
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.3):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+# Using Gaussian kernel function here to capture long-range dependencies
 def gaussian_kernel(qk, scale=1.0):
     return torch.exp(- (qk ** 2) / (2 * scale ** 2))
 
+# implementing Performer Attention to improve computation efficiency and scalability to some extent
 class PerformerAttention(nn.Module):
     def __init__(self, dim, heads=8, dim_head=64, dropout=0., kernel_fn=gaussian_kernel):
         super().__init__()
@@ -19,7 +35,7 @@ class PerformerAttention(nn.Module):
             nn.Dropout(dropout)
         )
 
-        self.kernel_fn = kernel_fn  # Kernel function for the Performer Attention (ReLU is common)
+        self.kernel_fn = kernel_fn
         
     def forward(self, x):
         batch_size, seq_len, dim = x.size()
